fe2eb3c0f6b762853fe747c00649c415a8389b88
SmokingGunz <ericowens08@gmail.com>
2023-09-17 21:38:59 -0700

    completed section 2.10

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,7 @@
+let country = 'United States';
+let continent = 'North America';
+let population = 1000000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,25 @@
 let js = 'amazing';
 console.log(40 + 8 + 23 - 10);
 
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Jonas";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = 'jm';
+let $function = 27;
+
+let PI = 3.141592653589793;
+
+let myFirstJob = 'Coder';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'programmer';
+let job2 = 'teacher';
+
+console.log(myFirstJob);
