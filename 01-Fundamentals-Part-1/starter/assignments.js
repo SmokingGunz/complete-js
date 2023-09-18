8ead9762852232923523bb25b529281dac9123bb
SmokingGunz <ericowens08@gmail.com>
2023-09-17 22:33:17 -0700

    completed section 2.11

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -5,3 +5,11 @@ let population = 1000000;
 console.log(country);
 console.log(continent);
 console.log(population);
+
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,4 @@
+/*
 let js = 'amazing';
 console.log(40 + 8 + 23 - 10);
 
@@ -11,6 +12,7 @@ console.log(firstName);
 console.log(firstName);
 console.log(firstName);
 
+// variable name conventions
 let jonas_matilda = 'jm';
 let $function = 27;
 
@@ -23,3 +25,24 @@ let job1 = 'programmer';
 let job2 = 'teacher';
 
 console.log(myFirstJob);
+*/
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
