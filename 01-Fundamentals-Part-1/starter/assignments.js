7b92c3d64270f5418f48fd2b9ba22ce8bf72c026
SmokingGunz <ericowens08@gmail.com>
2023-09-18 23:58:57 -0700

    completed section 2.17

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,15 +1,36 @@
-let country = 'United States';
-let continent = 'North America';
+const country = 'United States';
+const continent = 'North America';
 let population = 1000000;
+const description = 'Portugal is in Europe and its 11 million people speak portuguese.'
 
 console.log(country);
 console.log(continent);
 console.log(population);
 
-let isIsland = false;
+const isIsland = false;
 let language;
 
+language = 'English';
 console.log(typeof isIsland);
 console.log(typeof population);
 console.log(typeof country);
 console.log(typeof language);
+
+console.log(population / 2);
+let halfPopulation = population / 2;
+console.log(halfPopulation);
+
+population++;
+console.log(population);
+
+halfPopulation++;
+halfPopulation += 5;
+console.log(halfPopulation);
+
+let findlandPopulation = 6000000;
+console.log(population > findlandPopulation);
+console.log(population > 33000000);
+
+const countryPopulation = 33000000;
+let isMyPopulationBigger = population > countryPopulation;
+console.log(isMyPopulationBigger);
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -25,7 +25,6 @@ let job1 = 'programmer';
 let job2 = 'teacher';
 
 console.log(myFirstJob);
-*/
 
 let javascriptIsFun = true;
 console.log(javascriptIsFun);
@@ -46,3 +45,97 @@ year = 1991;
 console.log(typeof year);
 
 console.log(typeof null);
+
+// use let when declaring a mutable variable
+let age = 30;
+age = 31;
+
+// use const when declaring a constant variable
+const birthYear = 1991;
+
+// not a valid expression
+// birthYear = 1992;
+
+var job = 'programmer';
+job = 'teacher';
+
+lastName = 'Owens';
+console.log(lastName);
+
+// math operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 *2 
+
+const firstName = 'Jonas';
+const lastName = 'Shmedtmann';
+console.log(firstName + ' ' + lastName);
+
+// assignment operators
+let x = 10 + 5;
+x += 10; // x = x + 10 = 25;
+x *= 4; // x = x * 4 = 100;
+x++; // x = x + 1 = 101;
+x--; // x = x - 1 = 100;
+x--; // x = x - 1 = 99;
+console.log(x);
+
+// Copmarison operators
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+console.log(25 - 10 - 5);
+
+let x, y;
+
+x = y = 25 - 10 - 5; // x = y = 10;
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
+
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.95;
+
+const massMark = 95;
+const heightMark = 1.88;
+const massJohn = 85;
+const heightJohn = 1.76;
+
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+
+console.log(BMIMark, BMIJohn);
+
+const markHigherBMI = BMIJohn < BMIMark;
+console.log(markHigherBMI);
+*/
+
+const firstName = 'Jonas';
+const job = 'teacher';
+const birthYear = 1991;
+const year = 2037;
+
+const jonas = "I'm " + firstName + ', a ' + (year - birthYear) + ' year old ' + job + '!';
+
+console.log(jonas);
+
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
+
+console.log(jonasNew);
