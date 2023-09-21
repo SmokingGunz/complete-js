adaa01dfb79a593b6db304a37774f65aa220d104
SmokingGunz <ericowens08@gmail.com>
2023-09-20 23:59:41 -0700

    completed section 2.20

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -185,5 +185,9 @@ console.log(String(23), 23);
 // type coercion 
 console.log('I am ' + 23 + ' years old');
 console.log('I am ' + '23' + ' years old');
-console.log('23' - '10' - 3)
-console.log('23' * '2')
+console.log('23' - '10' - 3);
+console.log('23' * '2');
+
+let n = '1' + 1;
+n = n - 1;
+console.log(n);
