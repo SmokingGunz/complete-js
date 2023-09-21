3fac0f67b9be01a1ed4fa4d7a82a493e1166c9b1
SmokingGunz <ericowens08@gmail.com>
2023-09-20 23:57:56 -0700

    completed section 2.20

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -149,7 +149,7 @@ lines');
 console.log(`String with 
 multiple 
 lines `);
-*/
+
 
 const age = 6;
 
@@ -170,3 +170,20 @@ if (birthYear <= 2000) {
 }
 
 console.log(century);
+*/
+
+// type conversion 
+const inputYear = '1991';
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number('Jonas'));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+// type coercion 
+console.log('I am ' + 23 + ' years old');
+console.log('I am ' + '23' + ' years old');
+console.log('23' - '10' - 3)
+console.log('23' * '2')
