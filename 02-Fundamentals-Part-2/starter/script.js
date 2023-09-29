7c25beba323c65ad932540f1b3dd32cfd39d311e
SmokingGunz <ericowens08@gmail.com>
2023-09-28 23:46:00 -0700

    completed section 3.37

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -77,3 +77,19 @@ console.log(yearsUntilRetirement(1986, 'Bob'));
 
 // 3.36 Functions Calling Other Functions
 
+function cutFruitPieces(fruit) {
+    return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+
+    const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange`;
+    return juice;
+}
+
+console.log(fruitProcessor(2, 3));
+
+
