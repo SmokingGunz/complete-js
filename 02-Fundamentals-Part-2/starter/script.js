f88d415bf597d14fbb2814fc1e0c78160f04e84e
SmokingGunz <ericowens08@gmail.com>
2023-10-12 00:55:14 -0700

    completed section 3.47

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -502,8 +502,40 @@ const jonasArray = [
     true
 ];
 
+const types = [];
+
 for (let i = 0; i < jonasArray.length; i++) {
+    // Reading from jonasArray
+    console.log(jonasArray[i], typeof jonasArray[i]);
+
+    // Filling types array
+    // types[i] = typeof jonasArray[i];
+    types.push(typeof jonasArray[i]);
+}
+
+console.log(types);
+
+// Filling ages array with years through for loop
+
+const years = [1991, 2007, 1969, 2020];
+const ages = [];
+
+for (let i = 0; i < years.length; i++) {
+    ages.push(2037 - years[i]);
+}
+
+console.log(ages);
+
+// continue and break
+console.log(`----- ONLY STRINGS -----`);
+for (let i = 0; i < jonasArray.length; i++) {
+    if (typeof jonasArray[i] !== 'string') continue; // this skips over non-string values
     console.log(jonasArray[i], typeof jonasArray[i]);
 }
 
+console.log(`----- BREAK WITH NUMBER -----`);
+for (let i = 0; i < jonasArray.length; i++) {
+    if (typeof jonasArray[i] === 'number') break; // Breaks when a number is found
+    console.log(jonasArray[i], typeof jonasArray[i]);
+}
 
