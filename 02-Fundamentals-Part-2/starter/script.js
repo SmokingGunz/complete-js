4aaa52108582245c2db9adcee01b8450b40d2add
SmokingGunz <ericowens08@gmail.com>
2023-10-14 23:19:28 -0700

    update

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -539,5 +539,8 @@ for (let i = 0; i < jonasArray.length; i++) {
     console.log(jonasArray[i], typeof jonasArray[i]);
 }
 
-
-
+console.log(`----- CONTINUE WITH NUMBER -----`);
+for (let i = 0; i < jonasArray.length; i++) {
+    if (typeof jonasArray[i] !== 'number') continue; // Continues when a number is found
+    console.log(jonasArray[i], typeof jonasArray[i]);
+}
