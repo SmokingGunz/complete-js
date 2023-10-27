e732171127b6db17fec6acb800da1767d11a74bf
SmokingGunz <ericowens08@gmail.com>
2023-10-26 20:01:17 -0700

    update

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -544,10 +544,3 @@ for (let i = 0; i < jonasArray.length; i++) {
     if (typeof jonasArray[i] !== 'number') continue; // Continues when a number is found
     console.log(jonasArray[i], typeof jonasArray[i]);
 }
-
-
-
-// Challenge
-
-
-// "Jonas is a 46-year old teacher, and he has a/or no drivers license."
