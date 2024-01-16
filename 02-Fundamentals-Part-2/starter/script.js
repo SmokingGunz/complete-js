51b11c8d2cca47c52106f1a884668690b1c00659
SmokingGunz <ericowens08@gmail.com>
2024-01-16 15:11:55 -0800

    section 3.50 complete

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -619,3 +619,29 @@ Write a function calcAverage which takes an array called arr as an argument. Thi
 
 3. Call the function with the totals array.
 */
+
+const calcTip = function (bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+
+const tips = [];
+const totals = [];
+
+for (let i = 0; i < bills.length; i++) {
+    const tip = calcTip(bills[i]);
+    tips.push(tip);
+    totals.push(bills[i] + tip);
+}
+console.log(tips);
+console.log(totals);
+console.log(bills);
+
+const calcAverage = function (arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum / arr.length;
+}; 
