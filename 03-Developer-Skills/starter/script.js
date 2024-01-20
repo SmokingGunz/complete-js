8ad4d1690e735d930860e43f00381d6caf2faaf8
SmokingGunz <ericowens08@gmail.com>
2024-01-20 11:07:49 -0800

    completed section 5.59

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,6 +1,7 @@
 // Remember, we're gonna use strict mode in all scripts now!
-"use strict";
-
+'use strict';
+/**
+ * 
 const x = 23;
 
 let y = `23`;
@@ -32,3 +33,15 @@ while (sum < 100) {
     console.log(sum);
     sum++;
 }
+*/
+
+// 5.59 Using Google, Stack Overflow and MDN
+
+// PROBLEM;
+// WE work for a company building a smart home thermometer. Our most recent task is this: "Given an array of temperatures of one day, calculate the temperaature amplitude. Keep in mind that sometimes there might be a sensor error."
+
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+
+// 1) Understanding the problem
+
+// 2) Breaking up into sub-problems
