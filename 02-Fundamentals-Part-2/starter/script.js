f73890bc8f916a8a6f6d1f56b872cdfa71006239
SmokingGunz <ericowens08@gmail.com>
2024-01-19 23:24:40 -0800

    completed section Fundementals part 2

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1015,4 +1015,25 @@ for (let exercise = 1; exercise < 4; exercise++) {
 }
 */
 
-//
+// 3.49 While Loop
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`Lifting weights repetition ${i} ���️����️`)
+// }
+
+let rep = 1;
+while (rep <= 10) {
+    console.log(`Lifting weights repetition ${rep} ���️����️`);
+    rep++;
+}
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+
+while (dice !== 6) {
+    console.log(`You rolled a ${dice}!`);
+    dice = Math.trunc(Math.random() * 6) + 1;
+    if (dice === 6) {
+        console.log('Loop is about to end...');
+        break;
+    }
+}
